171de04cdb3b4e8719c381079422adc74550d994
sherman.xie <zhenshengxie1993@gmail.com>
2019-01-11 01:34:42 +0800

    modify filenames by adding [1] [2] [3]...infront of the filename at specified dir

--- rename_files.py
+++ rename_files.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
-print("hello world")
+import os
+if_cur_dir = input("input 1 for current dir or input the dir with like 'c:\\\\user\\\\...'")
+tar_extension = input("input the target file's extension like '.txt'")
+if if_cur_dir == '1':
+    path = os.getcwd()
+else:
+    path = if_cur_dir
+try:
+    filenames = os.listdir(path)
+except FileNotFoundError as e:
+    print(e.massege)
+cnt = 0
+for item in filenames:
+    extension = os.path.splitext(item)[1]
+    if extension == tar_extension:
+        cnt = cnt + 1
+        new_name = '[' + str(cnt) + '] ' + item
+        os.renames(path + '\\' + item, path + '\\' + new_name)
+print('%d filenames modified' % cnt)
+
+
